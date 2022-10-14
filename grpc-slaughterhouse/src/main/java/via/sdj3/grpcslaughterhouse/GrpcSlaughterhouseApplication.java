4d4b46866c28b7ac890c61f6c943935778513fc5
KamilFischbach <92295061+KamilFischbach@users.noreply.github.com>
2022-10-14 19:37:42 +0200

    Set up of gRPC project.

---
+++ grpc-slaughterhouse/src/main/java/via/sdj3/grpcslaughterhouse/GrpcSlaughterhouseApplication.java
@@ -0,0 +1,13 @@
+package via.sdj3.grpcslaughterhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcSlaughterhouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcSlaughterhouseApplication.class, args);
+	}
+
+}
---
+++ grpc-slaughterhouse/src/test/java/via/sdj3/grpcslaughterhouse/GrpcSlaughterhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package via.sdj3.grpcslaughterhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcSlaughterhouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
