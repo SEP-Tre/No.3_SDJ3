8dda8bd4ff97631be3400baf3b2c8e59f3e32a9d
chrfoyer <chrfoyer@gmail.com>
2022-10-25 15:59:31 +0200

    Working view and values
    The view now shows the details of each product going all the way back to the potential animals

--- grpc-slaughterhouse/slaughterhouse.sql
+++ grpc-slaughterhouse/slaughterhouse.sql
@@ -1,34 +1,79 @@
+DROP SCHEMA IF EXISTS slaughterhouse CASCADE ;
 CREATE SCHEMA IF NOT EXISTS slaughterhouse;
 SET SCHEMA 'slaughterhouse';
 
-CREATE TABLE IF NOT EXISTS part(
-    part_id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS part
+(
+    part_id   SERIAL PRIMARY KEY,
     animal_id int,
     part_name char(40),
-    weight float
+    weight    float
 );
 
-CREATE TABLE IF NOT EXISTS tray(
-    tray_id SERIAL PRIMARY KEY,
-    part_id int,
-    part_name char(32),
+CREATE TABLE IF NOT EXISTS tray
+(
+    tray_id         int,
+    part_id         int,
+    part_name       char(32),
     weight_capacity float,
 
-    FOREIGN KEY (part_id) REFERENCES part(part_id)
+    PRIMARY KEY (tray_id, part_id),
+    FOREIGN KEY (part_id) REFERENCES part (part_id)
 );
 
-CREATE TABLE IF NOT EXISTS product(
+CREATE TABLE IF NOT EXISTS product
+(
     product_id SERIAL PRIMARY KEY,
-    tray_id int,
-    type char(50),
-
-    FOREIGN KEY (tray_id) REFERENCES tray(tray_id)
+    tray_id    int,
+    type       char(50)
 );
 
 CREATE VIEW product_trace AS
-    SELECT *
-    FROM part, tray, product
-    JOIN tray t on product.tray_id = t.tray_id
-    JOIN slaughterhouse.part p ON p.part_id = t.part_id;
+SELECT type,
+       product.tray_id,
+       t.part_name,
+       t.part_id,
+       p.animal_id,
+       p.weight
+
+       FROM product
+INNER JOIN tray t ON product.tray_id = t.tray_id
+INNER JOIN part p ON p.part_id = t.part_id;
+
+
+INSERT INTO part (animal_id, part_name, weight)
+VALUES (1, 'leg', 1.5),
+       (1, 'shank', 3.1),
+       (1, 'ribs', 4.3),
+       (2, 'ribeye', 0.7),
+       (2, 't-bone', 2.4),
+       (2, 'shank', 2.1),
+       (3, 'ribs', 2.2),
+       (3, 'ribeye', 1.5),
+       (3, 'leg', 4.2);
+
+INSERT INTO tray (tray_id, part_id, part_name, weight_capacity)
+VALUES (0, 1, 'leg', 50),
+       (0, 9, 'leg', 50),
+       (1, 2, 'shank', 50),
+       (1, 6, 'shank', 50),
+       (2, 3, 'ribs', 60),
+       (2, 7, 'ribs', 60),
+       (3, 4, 'ribeye', 75),
+       (3, 8, 'ribeye', 75);
+
+INSERT INTO product (tray_id, type)
+VALUES (0, 'tasty leg'),
+       (0, 'tzaziki leg dip'),
+       (1, 'slow roasted shank'),
+       (1, 'bbq marinated shank'),
+       (2, 'south carolina style ribs'),
+       (2, 'kansas city ribs'),
+       (3, '5A ribeye'),
+       (3, 'black angus ribeye');
+
+SELECT *
+FROM product_trace;
+
 
 
