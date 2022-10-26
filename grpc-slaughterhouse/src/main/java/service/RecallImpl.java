e28ede6ec220881a89d998bbc23be88391ccddb1
Robert Barta <robert.barta314@gmail.com>
2022-10-26 09:13:55 +0200

    Create RecallImpl.java

---
+++ grpc-slaughterhouse/src/main/java/service/RecallImpl.java
@@ -0,0 +1,6 @@
+package service;
+
+public class RecallImpl extends RecallImplBase{
+}
+//for each GRPC class, one implementation class will extend it.
+// Use Kamil's pastebin for inspiration on how to do the implementation here
